5e1412758efd95b838744d71f6297c218c3fd994
Joel Stella <stella.joelivan@gmail.com>
2021-10-15 12:39:00 -0300

    Agrego login

--- mesa-ayuda/routes.js
+++ mesa-ayuda/routes.js
@@ -13,4 +13,17 @@ routes.post("/user", (req, res)=>{
     })
 })
 
+routes.get("/login", (req, res)=>{
+    req.getConnection((err, conn)=>{
+        if(err) return res.send(err)
+
+        conn.query("SELECT * FROM usuario WHERE usuario = '"+req.query.usuario+"' AND pass = '"+req.query.pass+"'", (err, rows)=>{
+            if(err) return res.send(err)
+
+            if(rows.length == 0) res.send(false)
+            else res.send(true)
+        })
+    })
+})
+
 module.exports = routes

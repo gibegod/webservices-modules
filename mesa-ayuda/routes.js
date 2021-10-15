b2ea7a4017b04d63228c0b4334feec43d883ed3d
Joel Stella <stella.joelivan@gmail.com>
2021-10-15 15:56:25 -0300

    Endpoints de reclamos

--- mesa-ayuda/routes.js
+++ mesa-ayuda/routes.js
@@ -26,4 +26,40 @@ routes.get("/login", (req, res)=>{
     })
 })
 
+routes.get("/reclamos", (req, res)=>{
+    req.getConnection((err, conn)=>{
+        if(err) return res.send(err)
+
+        conn.query("SELECT * FROM reclamo", (err, rows)=>{
+            if(err) return res.send(err)
+
+            res.send(rows)
+        })
+    })
+})
+
+routes.get("/reclamos/filtrar", (req, res)=>{
+    req.getConnection((err, conn)=>{
+        if(err) return res.send(err)
+
+        conn.query("SELECT * FROM reclamo WHERE estado = '"+req.query.estado+"'", (err, rows)=>{
+            if(err) return res.send(err)
+
+            res.send(rows)
+        })
+    })
+})
+
+routes.put("/reclamos/atender", (req, res)=>{
+    req.getConnection((err, conn)=>{
+        if(err) return res.send(err)
+
+        conn.query("UPDATE reclamo SET estado = '"+req.query.estado+"' WHERE idReclamo = "+req.query.idReclamo, (err, rows)=>{
+            if(err) return res.send(err)
+
+            res.send(rows)
+        })
+    })
+})
+
 module.exports = routes

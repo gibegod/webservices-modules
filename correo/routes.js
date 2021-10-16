ab1930071331fb90963a340caa395bae922ca78c
Joel Stella <stella.joelivan@gmail.com>
2021-10-16 01:59:37 -0300

    Traer envíos y actualizar estado

--- correo/routes.js
+++ correo/routes.js
@@ -33,11 +33,35 @@ routes.post("/envio", (req, res)=>{
         if(err) return res.send(err)
 
         const uuidV1 = uuid.v1()
-        
-        conn.query("INSERT INTO envio (id_venta, dni_destinatario, cod_seguimiento, estado) VALUES ("+req.query.id_venta+", '"+req.query.dni_destinatario+"', '"+uuidV1+"', "+"'INICIADO'"+")", (err, rows)=>{
+
+        conn.query("INSERT INTO envio (id_venta, dni_destinatario, cod_seguimiento, estado) VALUES ("+req.query.id_venta+", '"+req.query.dni_destinatario+"', '"+uuidV1+"', "+"'EN PREPARACIÓN'"+")", (err, rows)=>{
             if(err) return res.send(err)
 
             res.send(uuidV1)
+        })
+    })
+})
+
+routes.patch("/envio", (req, res)=>{
+    req.getConnection((err, conn)=>{
+        if(err) return res.send(err)
+
+        conn.query("UPDATE envio SET estado = '"+req.query.estado+"' WHERE cod_seguimiento = '"+req.query.cod_seguimiento+"'", (err, rows)=>{
+            if(err) return res.send(err)
+
+            res.send(rows)
+        })
+    })
+})
+
+routes.get("/envios", (req, res)=>{
+    req.getConnection((err, conn)=>{
+        if(err) return res.send(err)
+
+        conn.query("SELECT * FROM envio", (err, rows)=>{
+            if(err) return res.send(err)
+
+            res.json(rows)
         })
     })
 })

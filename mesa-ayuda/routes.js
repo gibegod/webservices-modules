74ae5f03b5ddcebfaf25ba739acd956c81318727
luanaselena <luanaselenatraversa@gmail.com>
2021-10-26 00:02:17 -0300

    swagger de mesa de ayuda

--- mesa-ayuda/routes.js
+++ mesa-ayuda/routes.js
@@ -1,6 +1,28 @@
 const express = require("express")
 const routes = express.Router()
 
+/**
+ *  @swagger
+ *  /user:
+ *     post:
+ *      description: Crea un nuevo usuario.
+ *      parameters:
+ *        - in: body
+ *          description: Se requieren los datos del usuario
+ *          schema:
+ *            type: object
+ *            required:
+ *              - usuario
+ *              - contrasenia
+ *            properties:
+ *              usuario:
+ *                type: string
+ *              contrasenia:
+ *                type: string
+ *      responses:
+ *          '200':
+ *             description: Successfull response
+ */
 routes.post("/user", (req, res)=>{
     req.getConnection((err, conn)=>{
         if(err) return res.send(err)
@@ -13,6 +35,28 @@ routes.post("/user", (req, res)=>{
     })
 })
 
+/**
+ *  @swagger
+ *  /login:
+ *     get:
+ *      description: Realiza el login.
+ *      parameters:
+ *        - in: body
+ *          description: Se requieren los datos del usuario
+ *          schema:
+ *            type: object
+ *            required:
+ *              - usuario
+ *              - contrasenia
+ *            properties:
+ *              usuario:
+ *                type: string
+ *              contrasenia:
+ *                type: string
+ *      responses:
+ *          '200':
+ *             description: Successfull response
+ */
 routes.get("/login", (req, res)=>{
     req.getConnection((err, conn)=>{
         if(err) return res.send(err)
@@ -28,6 +72,15 @@ routes.get("/login", (req, res)=>{
 
 ////////////////// Reclamos //////////////////
 
+/**
+ *  @swagger
+ *  /reclamos:
+ *     get:
+ *      description: Se carga un nuevo reclamo.
+ *      responses:
+ *          '200':
+ *             description: Successfull response
+ */
 routes.get("/reclamos", (req, res)=>{
     req.getConnection((err, conn)=>{
         if(err) return res.send(err)
@@ -40,6 +93,25 @@ routes.get("/reclamos", (req, res)=>{
     })
 })
 
+/**
+ *  @swagger
+ *  /reclamos/filtrar:
+ *     get:
+ *      description: Se filtran reclamos.
+ *      parameters:
+ *        - in: body
+ *          description: Se requiere el estado como filtro
+ *          schema:
+ *            type: object
+ *            required:
+ *              - estado
+ *            properties:
+ *              estado:
+ *                type: string
+ *      responses:
+ *          '200':
+ *             description: Successfull response
+ */
 routes.get("/reclamos/filtrar", (req, res)=>{
     req.getConnection((err, conn)=>{
         if(err) return res.send(err)
@@ -52,6 +124,28 @@ routes.get("/reclamos/filtrar", (req, res)=>{
     })
 })
 
+/**
+ *  @swagger
+ *  /reclamos/atender:
+ *     put:
+ *      description: Se atiende un reclamo.
+ *      parameters:
+ *        - in: body
+ *          description: Se requiere el comentario de resolucion y el id del reclamo
+ *          schema:
+ *            type: object
+ *            required:
+ *              - comentarioResolucion
+ *              - idReclamo
+ *            properties:
+ *              comentarioResolucion:
+ *                type: string
+ *              idReclamo:
+ *                type: integer
+ *      responses:
+ *          '200':
+ *             description: Successfull response
+ */
 routes.put("/reclamos/atender", (req, res)=>{
     req.getConnection((err, conn)=>{
         if(err) return res.send(err)
@@ -65,6 +159,16 @@ routes.put("/reclamos/atender", (req, res)=>{
 })
 
 ////////////////// Denuncias //////////////////
+
+/**
+ *  @swagger
+ *  /denuncias:
+ *     get:
+ *      description: Se obtienen todas las denuncias.
+ *      responses:
+ *          '200':
+ *             description: Successfull response
+ */
 routes.get("/denuncias", (req, res)=>{
     req.getConnection((err, conn)=>{
         if(err) return res.send(err)
@@ -77,6 +181,25 @@ routes.get("/denuncias", (req, res)=>{
     })
 })
 
+/**
+ *  @swagger
+ *  /denuncias/filtrar:
+ *     get:
+ *      description: Se filtran denuncias.
+ *      parameters:
+ *        - in: body
+ *          description: Se requiere el estado como filtro
+ *          schema:
+ *            type: object
+ *            required:
+ *              - estado
+ *            properties:
+ *              estado:
+ *                type: string
+ *      responses:
+ *          '200':
+ *             description: Successfull response
+ */
 routes.get("/denuncias/filtrar", (req, res)=>{
     req.getConnection((err, conn)=>{
         if(err) return res.send(err)
@@ -89,6 +212,28 @@ routes.get("/denuncias/filtrar", (req, res)=>{
     })
 })
 
+/**
+ *  @swagger
+ *  /denuncias/atender:
+ *     put:
+ *      description: Se atiende una denuncia.
+ *      parameters:
+ *        - in: body
+ *          description: Se requiere el comentario de resolucion y el id de la denuncia
+ *          schema:
+ *            type: object
+ *            required:
+ *              - comentarioResolucion
+ *              - idDenuncia
+ *            properties:
+ *              comentarioResolucion:
+ *                type: string
+ *              idDenuncia:
+ *                type: integer
+ *      responses:
+ *          '200':
+ *             description: Successfull response
+ */
 routes.put("/denuncias/atender", (req, res)=>{
     req.getConnection((err, conn)=>{
         if(err) return res.send(err)
--- mesa-ayuda/server.js
+++ mesa-ayuda/server.js
@@ -1,7 +1,8 @@
 const express = require("express")
 const mysql = require("mysql")
 const myconn = require("express-myconnection")
-
+const swaggerJsDoc = require('swagger-jsdoc');
+const swaggerUi = require('swagger-ui-express');
 const routes = require("./routes") 
 
 const app = express()
@@ -10,9 +11,23 @@ const dbOptions = {
     host : "localhost",
     port : 3306,
     user : "root",
-    password : "",
+    password : "root",
     database : "bd-webservice"
 }
+
+const swaggerOptions = {
+    swaggerDefinition: {
+        info: {
+            title: 'Mesa de ayuda',
+            description: 'Documentacion del modulo Mesa de Ayuda',
+            servers: ["http://localhost:9000"]
+        }
+    },
+    apis: ["./routes.js"]
+};
+
+const swaggerDocs = swaggerJsDoc(swaggerOptions);
+app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(swaggerDocs));
 
 app.use(myconn(mysql, dbOptions, "single"))
 app.use(express.json())
